605d9c3626f6aac3a352dca5f7f0d8087dd84fb6
Bruno Ibiapina <brunoibiapina@me.com>
2025-05-25 19:50:37 -0300

    Deploy final para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Site-Personalizado/', // isso é o NOME DO SEU REPOSITÓRIO no GitHub
   build: {
     target: 'es2018',
     outDir: 'dist',
   },
-})
-
+});
